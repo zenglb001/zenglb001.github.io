578d7e3426f25b2cc10c59228cf00aecf47262d4
zenglb <zenglb@126.com>
2019-08-18 02:38:30 +0800

    Update sf.js

--- fluid/sf.js
+++ fluid/sf.js
@@ -16,7 +16,7 @@ let PS_Copy = ShaderCommon + (function(){/**
   uniform sampler2D uSampler;
   varying vec2 vUV;
   void main() {
-    gl_FragColor = texture2D(uSampler,vUV);
+    gl_FragColor = vec4(1.0,0.0,0.0,1.0); texture2D(uSampler,vUV);
   }
 **/}).toString().slice(15,-5);
 
