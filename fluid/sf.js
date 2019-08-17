ce7ac8658ae5ae66fe2095176cfd526894641d44
zenglb <zenglb@126.com>
2019-08-18 02:26:30 +0800

    update

--- fluid/sf.js
+++ fluid/sf.js
@@ -1,7 +1,7 @@
-
 var RTSize = 512;
 let ShaderCommon = (function(){/**
 ivec2 texsize = ivec2(512,512);
+//ivec2 texsize = textureSize(uSampler,0);
 **/}).toString().slice(15,-5);
 
 let VS_Shader = ShaderCommon + (function(){/**
@@ -20,12 +20,6 @@ let PS_Copy = ShaderCommon + (function(){/**
   }
 **/}).toString().slice(15,-5);
 
-let PS_Clear = ShaderCommon + (function(){/**
-  void main() {
-    gl_FragColor = vec4(0.0,0.0,0.0,0.0);
-  }
-**/}).toString().slice(15,-5);
-
 //ps_advect
 let PS_Advect = ShaderCommon + (function(){/**
 precision highp float;
@@ -35,36 +29,12 @@ uniform sampler2D uSampler;
 uniform float uDeltaTime;
 
 void main(){
-    vec4 col = texture2D(uSampler,vUV);
-    vec2 duv = col.xy * vec2(texsize.y/texsize.x,1.0) * uDeltaTime; 
+    vec2 rdx = vec2(texsize.y/texsize.x,1.0); // 1 / grid scale
+
+    // follow the velocity field "back in time"
+    vec2 col = texture2D(uSampler,vUV).xy;
+    vec2 duv = col * rdx * uDeltaTime;
     gl_FragColor = vec4(texture2D(uSampler,vUV - duv).xy,0.0,1.0);
-}
-**/}).toString().slice(15,-5);
-
-//ps_fluid
-let PS_Fluid= ShaderCommon + (function(){/**
-precision mediump float;
-varying vec2 vUV;
-uniform sampler2D uSampler; //Color Texture RGB
-uniform sampler2D uSampler1; //Velocity texture2D RG32F
-
-uniform float uDeltaTime;
-
-//uniform float uForceExponent;
-//uniform vec2 uForceOrigin;
-//uniform float uTime;
-
-void main(){
-    //color advection
-    vec2 delta = texture2D(uSampler1,vUV).xy  * uDeltaTime;
-    vec3 color =texture2D(uSampler,vUV - delta).xyz;
-
-    //color added
-    // vec3 dye = clamp(sin(uTime * vec3(2.72, 5.12, 4.98))+0.5,0.0,1.0);
-    // float amp = exp(-uForceExponent * distance(uForceOrigin, vUV));
-    // color = mix(color, dye,  clamp(amp * 100.0,0.0,1.0));
-
-    gl_FragColor = vec4(color,1.0);
 }
 **/}).toString().slice(15,-5);
 
@@ -77,10 +47,8 @@ uniform sampler2D uSampler; //W_in 2D
 uniform float uDeltaTime;
 
 //map to [0,1.0]
-// uniform vec2 uForceOrigin;
-// uniform vec2 uForceVector;
-uniform vec3 uForceOrigin;
-uniform vec3 uForceVector;
+uniform vec3 uForceOrigin; //vec2
+uniform vec3 uForceVector; //vec2
 uniform float uForceExponent;
 
  //W_out 2D
@@ -92,8 +60,8 @@ void main(){
 }
 **/}).toString().slice(15,-5);
 
-//ps_jacobi1d
-let PS_Jacobi1d = ShaderCommon + (function(){/**
+//ps_jacobi
+let PS_Jacobi = ShaderCommon + (function(){/**
 precision highp float;
 varying vec2 vUV;
 uniform sampler2D uSampler; //1D X1
@@ -103,50 +71,41 @@ uniform float uAlpha;
 uniform float uBeta;
 
 void main(){
-    //ivec2 texsize = textureSize(uSampler,0);
-    float uoff = 1.0 / float(texsize.x);
-    float voff = 1.0 / float(texsize.y);
+    // left, right, bottom, and top x samples
+    vec2 uvoff = 1.0 / vec2(texsize);
+    vec2 wL = texture2D(uSampler, vUV - vec2(uvoff.x,0)).xy;
+    vec2 wR = texture2D(uSampler, vUV + vec2(uvoff.x,0)).xy;
+    vec2 wB = texture2D(uSampler, vUV - vec2(0,uvoff.y)).xy;
+    vec2 wT = texture2D(uSampler, vUV + vec2(0,uvoff.y)).xy;
 
-    float x1 = texture2D(uSampler,vUV - vec2(uoff,0)).x;
-    float x2 = texture2D(uSampler,vUV + vec2(uoff,0)).x;
-    float y1 = texture2D(uSampler,vUV - vec2(0,voff)).x;
-    float y2 = texture2D(uSampler,vUV + vec2(0,voff)).x;
-
-    float b1 = texture2D(uSampler1,vUV).x;
-
-    gl_FragColor = vec4((x1 + x2 +y1+y2 + uAlpha *b1) / uBeta, 0.0, 0.0, 1.0);
+    // b sample, from center
+    vec2 bC = texture2D(uSampler1, vUV).xy;
+    // evaluate Jacobi iteration
+    gl_FragColor = vec4((wL + wR + wB + wT + uAlpha * bC) / uBeta, 0.0, 1.0);
 }
 **/}).toString().slice(15,-5);
 
-//ps_jacobi2d
-let PS_Jacobi2d = ShaderCommon + (function(){/**
-precision mediump float;
+//ps_projdiv
+let PS_ProjDiv = ShaderCommon + (function(){/**
+precision highp float;
 varying vec2 vUV;
 
-uniform sampler2D uSampler; //2D X2
-uniform sampler2D uSampler1;//2D B2
-
-uniform float uAlpha;
-uniform float uBeta;
+uniform sampler2D uSampler; //W_in 2D 
 
 void main(){
-    //ivec2 texsize = textureSize(uSampler,0);
-    float uoff = 1.0/ float(texsize.x);
-    float voff = 1.0 / float(texsize.y);
+    vec2 uvoff = 1.0 / vec2(texsize);
+    vec2 wL = texture2D(uSampler, vUV - vec2(uvoff.x,0)).xy;
+    vec2 wR = texture2D(uSampler, vUV + vec2(uvoff.x,0)).xy;
+    vec2 wB = texture2D(uSampler, vUV - vec2(0,uvoff.y)).xy;
+    vec2 wT = texture2D(uSampler, vUV + vec2(0,uvoff.y)).xy;
 
-    vec2 x1 = texture2D(uSampler,vUV - vec2(uoff,0)).xy;
-    vec2 x2 = texture2D(uSampler,vUV + vec2(uoff,0)).xy;
-    vec2 y1 = texture2D(uSampler,vUV - vec2(0,voff)).xy;
-    vec2 y2 = texture2D(uSampler,vUV + vec2(0,voff)).xy;
-
-    vec2 b1 = texture2D(uSampler1,vUV).xy;
-
-    gl_FragColor = vec4((x1 + x2 +y1+y2 + uAlpha *b1) / uBeta,0.0,1.0);
+    float v = 0.5 * (wR.x - wL.x + wT.y - wB.y) / (uvoff.x);
+    gl_FragColor = vec4(vec2(v,v),0.0,1.0);
 }
 **/}).toString().slice(15,-5);
 
-//ps_projfinish
-let PS_ProjFinish = ShaderCommon + (function(){/**
+//ps_projgrad
+let PS_ProjGrad = ShaderCommon + (function(){/**
 precision mediump float;
 varying vec2 vUV;
 
@@ -154,43 +113,45 @@ uniform sampler2D uSampler; //1D P_in
 uniform sampler2D uSampler1; //2D W_in
 
 void main(){
-    //ivec2 texsize = textureSize(uSampler,0);
-    float uoff = 1.0 / float(texsize.x);
-    float voff = 1.0 / float(texsize.y);
+    vec2 uvoff = 1.0 / vec2(texsize);
+    vec2 wL = texture2D(uSampler, vUV - vec2(uvoff.x,0)).xy;
+    vec2 wR = texture2D(uSampler, vUV + vec2(uvoff.x,0)).xy;
+    vec2 wB = texture2D(uSampler, vUV - vec2(0,uvoff.y)).xy;
+    vec2 wT = texture2D(uSampler, vUV + vec2(0,uvoff.y)).xy;
 
-    float p1 = texture2D(uSampler,vUV - vec2(uoff,0)).x;
-    float p2 = texture2D(uSampler,vUV + vec2(uoff,0)).x;
-    float p3 = texture2D(uSampler,vUV - vec2(0,voff)).x;
-    float p4 = texture2D(uSampler,vUV + vec2(0,voff)).x;
-
-    vec2 u = texture2D(uSampler1,vUV).xy - vec2(p2-p1,p4-p3) /(2.0 * voff);
-
+    vec2 u = texture2D(uSampler1,vUV).xy;
+    u -= 0.5 * vec2(wR.x-wL.x, wT.x-wB.x) /(uvoff.y);
     gl_FragColor = vec4(u,0.0,1.0);
 }
 **/}).toString().slice(15,-5);
 
-//ps_projsetup
-let PS_ProjSetup = ShaderCommon + (function(){/**
-precision highp float;
+//ps_fluid
+let PS_Fluid= ShaderCommon + (function(){/**
+precision mediump float;
 varying vec2 vUV;
+uniform sampler2D uSampler; //Color Texture RGB
+uniform sampler2D uSampler1; //Velocity texture2D RG32F
 
-uniform sampler2D uSampler; //W_in 2D 
+uniform float uDeltaTime;
 
-//DivW_out 1D
+uniform float uForceExponent;
+uniform vec3 uForceOrigin;
+uniform float uTime;
+
 void main(){
-    //ivec2 texsize = textureSize(uSampler,0);
-    float uoff = 1.0/ float(texsize.x);
-    float voff = 1.0 / float(texsize.y);
+    //color advection
+    vec2 delta = texture2D(uSampler1,vUV).xy * uDeltaTime;
+    vec3 color = texture2D(uSampler,vUV - delta).xyz;
 
+    if(uTime > 0.0)
+    {
+        //color added
+        vec3 dye = clamp(sin(uTime * vec3(2.72, 5.12, 4.98))+0.5,0.0,1.0);
+        float amp = exp(-uForceExponent * distance(uForceOrigin.xy, vUV));
+        color = mix(color, dye,  clamp(amp * 100.0,0.0,1.0));
+    }
 
-    float x1 = texture2D(uSampler,vUV + vec2(uoff,0)).x;
-    float x2 = texture2D(uSampler,vUV - vec2(uoff,0)).x;
-
-    float y1 = texture2D(uSampler,vUV + vec2(0,voff)).y;
-    float y2 = texture2D(uSampler,vUV - vec2(0,voff)).y;
-
-    float v =  (x1 - x2 + y1 - y2) / (2.0 * uoff);
-    gl_FragColor = vec4(vec2(v,v),0.0,1.0);
+    gl_FragColor = vec4(color,1.0);
 }
 **/}).toString().slice(15,-5);
 
@@ -210,17 +171,15 @@ function SFluid() {
       this.texP1  = new GL.Texture(RTSize, RTSize, { type: gl.HALF_FLOAT_OES }); //R32F
       this.texP2  = new GL.Texture(RTSize, RTSize, { type: gl.HALF_FLOAT_OES }); //R32F
     }
-  
-  
+
     this.colRT1 = new GL.Texture(RTSize, RTSize); //RGBA8
     this.colRT2 = new GL.Texture(RTSize, RTSize); //RGBA8
 
     //init
     this.RTShaderCopy = new GL.Shader(VS_Shader, PS_Copy);
-    SFluid.prototype.updateCopy = function(src, dst) {
+    SFluid.prototype.updateCopy = function(dst, src) {
         var this_ = this;
         dst.drawTo(function() {
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             src.bind(0);
             this_.RTShaderCopy.uniforms({
                 uSampler : 0,
@@ -229,21 +188,17 @@ function SFluid() {
         });
     };
 
-    this.RTShaderClear = new GL.Shader(VS_Shader, PS_Clear);
     SFluid.prototype.updateClear = function(tex) {
-        var this_ = this;
         tex.drawTo(function() {
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-            this_.RTShaderClear.draw(this_.quad);
         });
     };
 
     //Advection
     this.RTShaderAdvect = new GL.Shader(VS_Shader, PS_Advect);
-    SFluid.prototype.updateAdvection = function(src, dst, deltaTime) {
+    SFluid.prototype.updateAdvection = function(dst, src, deltaTime) {
         var this_ = this;
         dst.drawTo(function() {
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             src.bind(0);
             this_.RTShaderAdvect.uniforms({
                 uDeltaTime: deltaTime,
@@ -254,32 +209,13 @@ function SFluid() {
     };
 
     //Jacobi
-    this.RTShaderJacobi1d = new GL.Shader(VS_Shader, PS_Jacobi1d);
-    SFluid.prototype.updateJacobi1d = function(src0, src1, dst, alpha, beta) {
+    this.RTShaderJacobi = new GL.Shader(VS_Shader, PS_Jacobi);
+    SFluid.prototype.updateJacobi = function(dst, src0, src1, alpha, beta) {
         var this_ = this;
         dst.drawTo(function() {
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             src0.bind(0);
             src1.bind(1);
-            this_.RTShaderJacobi1d.uniforms({
-                uSampler : 0,
-                uSampler1 : 1,
-                uAlpha : alpha,
-                uBeta : beta,
-              }).draw(this_.quad);
-              src0.unbind(0);
-              src1.unbind(1);
-        });
-    };
-
-    this.RTShaderJacobi2d = new GL.Shader(VS_Shader, PS_Jacobi2d);
-    SFluid.prototype.updateJacobi2d = function(src0, src1, dst, alpha, beta) {
-        var this_ = this;
-        dst.drawTo(function() {
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-            src0.bind(0);
-            src1.bind(1);
-            this_.RTShaderJacobi2d.uniforms({
+            this_.RTShaderJacobi.uniforms({
                 uSampler : 0,
                 uSampler1 : 1,
                 uAlpha : alpha,
@@ -292,43 +228,40 @@ function SFluid() {
 
     //Force
     this.RTShaderForce = new GL.Shader(VS_Shader, PS_Force);
-    SFluid.prototype.updateForce = function(src, dst, exponent, forceVec, inputVec) {
+    SFluid.prototype.updateForce = function(dst, src, exponent, forceVec, forceOri) {
         var this_ = this;
         dst.drawTo(function() {
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             src.bind(0);
             this_.RTShaderForce.uniforms({
                 uSampler : 0,
                 uForceExponent : exponent,
                 uForceVector : forceVec,
-                uForceOrigin : inputVec,
+                uForceOrigin : forceOri,
               }).draw(this_.quad);
               src.unbind(0);
         });
     };
 
     //Proj
-    this.RTShaderProjSetup = new GL.Shader(VS_Shader, PS_ProjSetup);
-    SFluid.prototype.updateProjSetup = function(src, dst) {
+    this.RTShaderProjDiv = new GL.Shader(VS_Shader, PS_ProjDiv);
+    SFluid.prototype.updateProjDiv = function(dst, src) {
         var this_ = this;
         dst.drawTo(function() {
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             src.bind(0);
-            this_.RTShaderProjSetup.uniforms({
+            this_.RTShaderProjDiv.uniforms({
                 uSampler : 0,
               }).draw(this_.quad);
               src.unbind(0);
         });
     };
 
-    this.RTShaderProjFinish = new GL.Shader(VS_Shader, PS_ProjFinish);
-    SFluid.prototype.updateProjFinish = function(src0, src1, dst) {
+    this.RTShaderProjGrad = new GL.Shader(VS_Shader, PS_ProjGrad);
+    SFluid.prototype.updateProjGrad = function(dst, src0, src1) {
         var this_ = this;
         dst.drawTo(function() {
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             src0.bind(0);
             src1.bind(1);
-            this_.RTShaderProjFinish.uniforms({
+            this_.RTShaderProjGrad.uniforms({
                 uSampler : 0,
                 uSampler1 : 1,
               }).draw(this_.quad);
@@ -339,19 +272,18 @@ function SFluid() {
 
     //Fluid
     this.RTShaderFluid = new GL.Shader(VS_Shader, PS_Fluid);
-    SFluid.prototype.updateFluid = function(src0, src1, dst, deltaTime, time, exponent, inputVec) {
+    SFluid.prototype.updateFluid = function(dst, src0, src1, deltaTime, time, exponent, inputVec) {
         var this_ = this;
         dst.drawTo(function() {
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             src0.bind(0);
             src1.bind(1);
             this_.RTShaderFluid.uniforms({
                 uSampler : 0,
                 uSampler1 : 1,
                 uDeltaTime : deltaTime,
-                //uTime : time,
-                //uForceExponent : exponent,
-                //uForceOrigin : inputVec,
+                uTime : time,
+                uForceExponent : exponent,
+                uForceOrigin : inputVec,
               }).draw(this_.quad);
               src0.unbind(0);
               src1.unbind(1);
